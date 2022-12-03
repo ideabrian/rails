252c5104fcd1b8c8fb9bcd30a09960f2a03e0761
Brian Ball <ideabrian@gmail.com>
2022-12-02 17:18:46 -0800

    Change the config.hosts to hard coded << 'rails.up.railway.app'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,11 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Rails.application.configure do
-    config.hosts = [
-      ENV["SERVER_HOST_NAME"]
-    ]
-  end
   
-
+    
+  config.hosts << "rails.up.railway.app"
 end
